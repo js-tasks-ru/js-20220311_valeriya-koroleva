aaecc4bc94d817c67cdd34e7d96411c99e646fc6
Valeria <Koroleva.Valeria.97@yandex.ru>
2022-03-22 18:05:59 +0300

    add createGetter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,10 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+    const arr = path.split('.');
+      return obj => {
+        let value = obj;
+        arr.forEach(property => value = value?.[property]);
+        return value;
+  };
 }
