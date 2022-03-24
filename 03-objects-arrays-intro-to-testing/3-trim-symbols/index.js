307b7b242bce1a6f3764f4134f26516171c7fbdb
Valeria <Koroleva.Valeria.97@yandex.ru>
2022-03-24 11:34:33 +0300

    add trimymbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,32 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+    if(size ===0) {
+        return '';
+    }
+    if (size === undefined) {
+        return string;
+    }
 
-}
+ const arr = [...string];
+ const result = [];
+ let counter = 0;
+ let identically = true;
+
+ arr.forEach((currentValue, index)=>{
+    if (index > 0) {
+        identically = arr[index] === arr[index - 1];
+    }
+    if (identically) {
+      if (counter < size) {
+        counter+=1;
+        result.push(currentValue);
+      }
+    } else {
+        counter = 1;
+        result.push(currentValue);
+    }
+  });
+  
+  return result.join('');
+  }
